e7bc6b6b3e6f854b8bd642cbfa309066e4cc3361
nattesharan <nattesharan@gmail.com>
2017-05-02 23:39:34 +0530

    Getting the data from rss feed

--- NewsNow.py
+++ NewsNow.py
@@ -1,7 +1,34 @@
+import feedparser
 from flask import Flask
 app = Flask(__name__)
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn':'http://rss.cnn.com/rss/edition.rss',
+             'fox':'http://feeds.foxnews.com/foxnews/latest',
+             'iol':'http://www.iol.co.za/cmlink/1.640'}
 @app.route('/')
 def home():
-    return '<html><body><h1><strong><i>There is no news</i></strong></h1></body></html>'
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return '''<html>
+              <body>
+              <h1> BBC Headlines </h1>
+              <b>{0}</b> <br/>
+              <i>{1}</i> <br/>
+              <p>{2}</p> <br/>
+              </body>
+              </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
+@app.route('/<publicationname>')
+def get_news(publicationname):
+    feed = feedparser.parse(RSS_FEEDS[publicationname])
+    first_article = feed['entries'][0]
+    return '''<html>
+              <body>
+              <h1> BBC Headlines </h1>
+              <b>{0}</b> <br/>
+              <i>{1}</i> <br/>
+              <p>{2}</p> <br/>
+              </body>
+              </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
 if __name__ == '__main__':
     app.run(port=int("3000"),debug = True)
