19c1ec617781673818cfc5122a6bf5d6564e7f01
nattesharan <nattesharan@gmail.com>
2017-05-02 23:49:32 +0530

    Added render_template method for adding view

--- NewsNow.py
+++ NewsNow.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
@@ -10,25 +10,27 @@ RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
 def home():
     feed = feedparser.parse(BBC_FEED)
     first_article = feed['entries'][0]
-    return '''<html>
-              <body>
-              <h1> BBC Headlines </h1>
-              <b>{0}</b> <br/>
-              <i>{1}</i> <br/>
-              <p>{2}</p> <br/>
-              </body>
-              </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
+    # return '''<html>
+    #           <body>
+    #           <h1> BBC Headlines </h1>
+    #           <b>{0}</b> <br/>
+    #           <i>{1}</i> <br/>
+    #           <p>{2}</p> <br/>
+    #           </body>
+    #           </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
+    return render_template('home.html',title = first_article.get('title'), published = first_article.get('published'), summary = first_article.get('summary'))
 @app.route('/<publicationname>')
 def get_news(publicationname):
     feed = feedparser.parse(RSS_FEEDS[publicationname])
     first_article = feed['entries'][0]
-    return '''<html>
-              <body>
-              <h1> BBC Headlines </h1>
-              <b>{0}</b> <br/>
-              <i>{1}</i> <br/>
-              <p>{2}</p> <br/>
-              </body>
-              </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
+    # return '''<html>
+    #           <body>
+    #           <h1> BBC Headlines </h1>
+    #           <b>{0}</b> <br/>
+    #           <i>{1}</i> <br/>
+    #           <p>{2}</p> <br/>
+    #           </body>
+    #           </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
+    return render_template('home.html',title = first_article.get('title'), published = first_article.get('published'), summary = first_article.get('summary'))
 if __name__ == '__main__':
     app.run(port=int("3000"),debug = True)
