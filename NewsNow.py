7aea52c0d2afbe91bccaa97fb6562389f15641dc
nattesharan <nattesharan@gmail.com>
2017-05-03 12:54:42 +0530

    Resolved the conflicts

--- NewsNow.py
+++ NewsNow.py
@@ -5,43 +5,27 @@ BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn':'http://rss.cnn.com/rss/edition.rss',
              'fox':'http://feeds.foxnews.com/foxnews/latest',
-             'iol':'http://www.iol.co.za/cmlink/1.640'}
-# @app.route('/')
-# def home():
-#     feed = feedparser.parse(BBC_FEED)
-#     # first_article = feed['entries'][0]
-#     # return '''<html>
-#     #           <body>
-#     #           <h1> BBC Headlines </h1>
-#     #           <b>{0}</b> <br/>
-#     #           <i>{1}</i> <br/>
-#     #           <p>{2}</p> <br/>
-#     #           </body>
-#     #           </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
-#     # return render_template('home.html',title = first_article.get('title'), published = first_article.get('published'), summary = first_article.get('summary'))
-#     return render_template('home.html', articles = feed['entries'])
-# @app.route('/<publicationname>')
-# def get_news(publicationname):
-#     feed = feedparser.parse(RSS_FEEDS[publicationname])
-#     # first_article = feed['entries'][0]
-#     # return '''<html>
-#     #           <body>
-#     #           <h1> BBC Headlines </h1>
-#     #           <b>{0}</b> <br/>
-#     #           <i>{1}</i> <br/>
-#     #           <p>{2}</p> <br/>
-#     #           </body>
-#     #           </html>'''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
-#     # return render_template('home.html',title = first_article.get('title'), published = first_article.get('published'), summary = first_article.get('summary'))
-#     return render_template('home.html', articles = feed['entries'])
+             'iol':'http://indiatoday.intoday.in/rss/homepage-topstories.jsp'}
 @app.route('/',methods = ['GET', 'POST'])
-def get_news():
-    publicationname = request.form['name']
-    if not publicationname or publicationname.lower() not in RSS_FEEDS:
-        publicationname = 'bbc'
+def home():
+    if request.method == 'POST':
+        publicationname = request.form['name'].lower()
+        for i in RSS_FEEDS:
+            if i == publicationname:
+                errors = False
+                break
+            else:
+                errors = True
+                publicationname = 'bbc'
+        if not errors:
+            articles = get_news(publicationname)
+        return render_template('home.html',articles = articles)
     else:
-        publicationname = request.form['name']
+        feeds =  feedparser.parse(RSS_FEEDS['bbc'])
+        articles = feeds['entries']
+        return render_template('home.html',articles = articles)
+def get_news(publicationname):
     feeds = feedparser.parse(RSS_FEEDS[publicationname])
-    return render_template('home.html', articles = feeds['entries'])
+    return feeds['entries']
 if __name__ == '__main__':
     app.run(port=int("3000"),debug = True)
